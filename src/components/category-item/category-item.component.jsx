59f46a4bdd0bcc3bcbe5820a560e842b0fc1cff6
nav33n <13643224+knav33n@users.noreply.github.com>
2022-08-11 11:19:31 +0530

    update components

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -1,16 +1,22 @@
-import "./category-item.styles.scss";
+import { useNavigate } from "react-router-dom";
+import "./category-item.styles.jsx";
+import {
+  Body,
+  DirectoryItemContainer,
+  BackgroundImage,
+} from "./category-item.styles.jsx";
 
-export default function CategoryItem({ category: { title, imageUrl } }) {
+export default function CategoryItem({ category: { title, imageUrl, route } }) {
+  const navigate = useNavigate();
+  const onNavigateHandler = () => navigate(route);
+
   return (
-    <div className="category-container">
-      <div
-        className="background-image"
-        style={{ backgroundImage: `url(${imageUrl})` }}
-      />
-      <div className="category-body-container">
+    <DirectoryItemContainer onClick={onNavigateHandler}>
+      <BackgroundImage imageUrl={imageUrl} />
+      <Body>
         <h2>{title}</h2>
         <p>Shop Now</p>
-      </div>
-    </div>
+      </Body>
+    </DirectoryItemContainer>
   );
 }
