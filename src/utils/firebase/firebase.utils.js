acef674c4290339b8ed8baee377b7c909ca1dfc6
nav33n <13643224+knav33n@users.noreply.github.com>
2022-08-13 20:17:37 +0530

    move firebase credentials to .env

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -19,8 +19,15 @@ import {
   query,
   getDocs,
 } from "firebase/firestore";
-import { firebaseConfig } from "../../config/secrets";
 
+export const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FS_APIKEY,
+  authDomain: process.env.REACT_APP_FS_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FS_PROJECTID,
+  storageBucket: process.env.REACT_APP_FS_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FS_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FS_APPID,
+};
 const firebaseApp = initializeApp(firebaseConfig);
 
 const googleProvider = new GoogleAuthProvider();
