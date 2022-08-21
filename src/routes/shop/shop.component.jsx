13593d9648000b5d2f4e905f17b67c4f43769070
nav33n <13643224+knav33n@users.noreply.github.com>
2022-08-21 21:14:46 +0530

    refactor categories to use sagas for fetching/async

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,7 +1,10 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Routes } from "react-router-dom";
-import { fetchCategoriesAsync } from "../../store/categories/category.action";
+import {
+  fetchCategoriesAsync,
+  fetchCategoriesStart,
+} from "../../store/categories/category.action";
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from "../category/category.component";
 import "./shop.styles.scss";
@@ -10,7 +13,7 @@ export default function Shop() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchCategoriesAsync());
+    dispatch(fetchCategoriesStart());
   }, [dispatch]);
 
   return (
